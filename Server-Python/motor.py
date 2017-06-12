a79441a506d335afb17961da1a64ab7c00daac4f
KimJeongWook <rkrn6131@naver.com>
2017-06-12 18:16:51 +0900

    mkdir
    move file to server folder

---
+++ Server-Python/motor.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import PCA9685 as p
+import time    # Import necessary modules
+import car_dir
+# ===========================================================================
+# Raspberry Pi pin11, 12, 13 and 15 to realize the clockwise/counterclockwise
+# rotation and forward and backward movements
+# ===========================================================================
+Motor0_A = 11  # pin11
+Motor0_B = 12  # pin12
+Motor1_A = 13  # pin13
+Motor1_B = 15  # pin15
+
+# ===========================================================================
+# Set channel 4 and 5 of the servo driver IC to generate PWM, thus 
+# controlling the speed of the car
+# ===========================================================================
+EN_M0    = 4  # servo driver IC CH4
+EN_M1    = 5  # servo driver IC CH5
+
+pins = [Motor0_A, Motor0_B, Motor1_A, Motor1_B]
+
+# ===========================================================================
+# Adjust the duty cycle of the square waves output from channel 4 and 5 of
+# the servo driver IC, so as to control the speed of the car.
+# ===========================================================================
+def setSpeed(speed):
+	speed *= 40
+	pwm.write(EN_M0, 0, speed)
+	pwm.write(EN_M1, 0, speed)
+
+def setup(busnum=None):
+	global forward0, forward1, backward1, backward0
+	global pwm
+	if busnum == None:
+		pwm = p.PWM()                  # Initialize the servo controller.
+	else:
+		pwm = p.PWM(bus_number=busnum) # Initialize the servo controller.
+
+	pwm.frequency = 60
+	forward0 = 'True'
+	forward1 = 'True'
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BOARD)        # Number GPIOs by its physical location
+	try:
+		for line in open("config"):
+			if line[0:8] == "forward0":
+				forward0 = line[11:-1]
+			if line[0:8] == "forward1":
+				forward1 = line[11:-1]
+	except:
+		pass
+	if forward0 == 'True':
+		backward0 = 'False'
+	elif forward0 == 'False':
+		backward0 = 'True'
+	if forward1 == 'True':
+		backward1 = 'False'
+	elif forward1 == 'False':
+		backward1 = 'True'
+	for pin in pins:
+		GPIO.setup(pin, GPIO.OUT)   # Set all pins' mode as output
+
+# ===========================================================================
+# Control the DC motor to make it rotate clockwise, so the car will 
+# move forward.
+# ===========================================================================
+
+
+def motor0(x):
+	if  x == 'True':
+		GPIO.output(Motor0_A, GPIO.LOW)
+		GPIO.output(Motor0_B, GPIO.HIGH)
+		
+	elif x == 'False':
+		GPIO.output(Motor0_A, GPIO.HIGH)
+		GPIO.output(Motor0_B, GPIO.LOW)
+	else:
+		print 'Config Error'
+
+def motor1(x):
+	
+
+	if x == 'True':
+		GPIO.output(Motor1_A, GPIO.LOW)
+		GPIO.output(Motor1_B, GPIO.HIGH)
+
+	elif x == 'False':
+		GPIO.output(Motor1_A, GPIO.HIGH)
+		GPIO.output(Motor1_B, GPIO.LOW)
+
+#========================================================================
+#When in automatic mode, Control the DC motor
+#======================================================================== 
+
+def forward_auto() :
+	motor0(forward0)
+	motor1(forward1)
+	time.sleep(0.20)
+	stop()
+#========================================================================
+#Make the car move forward
+#========================================================================
+def forward():
+	
+	motor0(forward0)
+	motor1(forward1)
+
+#========================================================================
+#Make the car move backward
+#========================================================================
+def backward():
+	motor0(backward0)
+	motor1(backward1)
+
+#========================================================================
+#When car detect obstacle, move backward
+#========================================================================
+
+def collision_auto():
+	car_dir.home()
+	backward()
+	stop()
+	backward()
+	time.sleep(0.6)
+	stop()
+#========================================================================
+#Make the car stop
+#========================================================================
+def stop():
+	for pin in pins:
+		GPIO.output(pin, GPIO.LOW)
+
+# ===========================================================================
+# The first parameter(status) is to control the state of the car, to make it 
+# stop or run. The parameter(direction) is to control the car's direction 
+# (move forward or backward).
+# ===========================================================================
+def ctrl(status, direction=1):
+	if status == 1:   # Run
+		if direction == 1:     # Forward
+			forward()
+		elif direction == -1:  # Backward
+			backward()
+		else:
+			print 'Argument error! direction must be 1 or -1.'
+	elif status == 0: # Stop
+		stop()
+	else:
+		print 'Argument error! status must be 0 or 1.'
+
+def test():
+	while True:
+		setup()
+		ctrl(1)
+		time.sleep(3)
+		setSpeed(10)
+		time.sleep(3)
+		setSpeed(100)
+		time.sleep(3)
+		ctrl(0)
+
+if __name__ == '__main__':
+	setup()
+	setSpeed(50)
+	stop()

2615f32da9b4c61232a9e4edeceb1acbcd72d972
KimJoengWook <rkrn6131@naver.com>
2017-06-12 18:47:04 +0900

    Create MainActivity.java

--- Client-Android/app/src/main/java/com/jeongwook/test/MainActivity.java
+++ Client-Android/app/src/main/java/com/jeongwook/test/MainActivity.java
@@ -21,10 +21,12 @@ public class MainActivity extends AppCompatActivity {
     public void button(View v){
         adress = (EditText) findViewById(R.id.ipaddress);
         if(adress.getText().toString().equals("192.168.0.13") || adress.getText().toString().equals("172.20.10.6")){
+            //라즈베리파이를 키면 자동으로 연결되는 IP주소 두개를 설정 해놓았다. 핫스팟을 사용했기 때문에 지정해놓았고 TCP SERVER에서 반응이있으면 연결 성공으로 해야 한다 
             Intent intent = new Intent(getApplicationContext(), StartActivity.class);
             intent.putExtra("ipaddress", adress.getText().toString());
             startActivity(intent);
             System.exit(1);
+            //새로운 Activity로 넘어가고 현재 Activity는 종료
         }else Toast.makeText(this, "틀린 IP주소 입니다.", Toast.LENGTH_SHORT).show();
     }
 
